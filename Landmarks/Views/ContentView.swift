767cbb46168d0b415223a341c2f5856304d208e9
Paul Leelaviwatana <apaulture@gmail.com>
2024-01-02 20:37:36 -0800

    Completed handling user input

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         LandmarkList()
-        
-        
     }
 }
 
 #Preview {
     ContentView()
+        .environment(ModelData())
 }
